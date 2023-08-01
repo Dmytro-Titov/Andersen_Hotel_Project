028ecbaae7fb4e6e81a23eef2427ae7157d203c7
Volodva <horsemajor02@gmail.com>
2023-08-01 23:06:58 +0300

    Copied Apartment

--- src/main/java/com/andersenlab/entity/Apartment.java
+++ src/main/java/com/andersenlab/entity/Apartment.java
@@ -1,4 +1,58 @@
 package com.andersenlab.entity;
 
+import java.util.Objects;
+
 public class Apartment {
+    private final Long id;
+    private double price;
+    private int capacity;
+    private boolean availability;
+
+    public Apartment(Long id, double price, int capacity) {
+        this.id = id;
+        this.price = price;
+        this.capacity = capacity;
+        this.availability = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isAvailable() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apartment apartment = (Apartment) o;
+        return Objects.equals(id, apartment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
