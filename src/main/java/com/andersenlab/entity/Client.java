18c128bf8edfc7e5ed4d299149ae6b4a4499d58c
Volodva <horsemajor02@gmail.com>
2023-08-01 12:59:58 +0300

    Client class pojo

--- src/main/java/com/andersenlab/entity/Client.java
+++ src/main/java/com/andersenlab/entity/Client.java
@@ -1,13 +1,24 @@
 package com.andersenlab.entity;
 
 
+import java.time.Instant;
+
 public class Client {
-    private final Long id;
+    private final long id;
     private String name;
 
-    public Client(String name, Long id) {
+    private Instant checkInDate;
+    private Instant checkOutDate;
+    private boolean status;
+
+    public Client(String name, long id) {
         this.id = id;
         this.name = name;
+        status = false;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getName() {
@@ -16,5 +27,29 @@ public class Client {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Instant checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Instant getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Instant checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
