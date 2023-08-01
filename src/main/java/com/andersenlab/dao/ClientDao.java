3adfb0eb82a2c2e8d5565ca04a77e9e2930b53a1
Volodva <horsemajor02@gmail.com>
2023-08-01 22:32:44 +0300

    Util Class + ClientDao(Singleton)

--- src/main/java/com/andersenlab/dao/ClientDao.java
+++ src/main/java/com/andersenlab/dao/ClientDao.java
@@ -3,14 +3,60 @@ package com.andersenlab.dao;
 import com.andersenlab.entity.Client;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class ClientDao {
-    private List<Client> clients;
+    private static ClientDao instance;
+    private List<Client> clients = new ArrayList<>();
 
-    public ClientDao() {
-        this.clients = new ArrayList<>();
+    private ClientDao() {}
+
+    public static ClientDao getInstance() {
+        if (instance == null) {
+            instance = new ClientDao();
+        }
+        return instance;
     }
 
+    public long addClient(Client newClient) {
+        clients.add(newClient);
+        return newClient.getId();
+    }
 
+    public Client getClientById(long id) {
+        return clients.stream().
+                filter(client -> client.getId() == id).
+                findFirst().
+                orElseThrow(() -> new NoSuchElementException("Client with ID " + id + " not found"));
+    }
+
+    public List<Client> getAllClients() {
+        return new ArrayList<>(clients);
+    }
+
+    public void removeClient(Client newClient) {
+        clients.remove(newClient);
+
+    }
+
+    public List<Client> sortByName(){
+        return clients.stream()
+                .sorted(Comparator.comparing(Client::getName))
+                .collect(Collectors.toList());
+    }
+
+    public List<Client> sortByCheckOutDate(){
+        return clients.stream()
+                .sorted(Comparator.comparing(Client::getCheckOutDate))
+                .collect(Collectors.toList());
+    }
+
+    public List<Client> sortByStatus() {
+        return clients.stream()
+                .sorted(Comparator.comparing(Client::isLives))
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/andersenlab/service/ClientService.java
+++ src/main/java/com/andersenlab/service/ClientService.java
@@ -1,4 +1,18 @@
 package com.andersenlab.service;
 
+import com.andersenlab.dao.ClientDao;
+import com.andersenlab.entity.Client;
+import com.andersenlab.util.IdGenerator;
+
 public class ClientService {
+    private ClientDao clientDao = ClientDao.getInstance();
+
+    public long createAndAddNewClient(String name) {
+        return clientDao.addClient(new Client(name, IdGenerator.generateClientId()));
+    }
+
+    public Client getClient(long id){
+        return clientDao.getClientById(id);
+    }
+
 }
