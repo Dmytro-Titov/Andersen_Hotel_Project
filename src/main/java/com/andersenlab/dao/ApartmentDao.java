a9a64c79511fca08d7e933cac81040307affe2cf
savitskidz <savitski.dz@gmail.com>
2023-08-02 12:35:25 +0200

    Changing Apartment Deleting ApartmentType Adding ApartmentDao, ApartmentService

--- src/main/java/com/andersenlab/dao/ApartmentDao.java
+++ src/main/java/com/andersenlab/dao/ApartmentDao.java
@@ -1,4 +1,42 @@
 package com.andersenlab.dao;
 
+import com.andersenlab.entity.Apartment;
+import com.andersenlab.entity.ApartmentStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApartmentDao {
+    private List<Apartment> apartments;
+
+    public ApartmentDao () {
+        this.apartments = new ArrayList<>();
+    }
+
+    public Apartment get(int id) {
+        for (Apartment apartment: apartments) {
+            if (apartment.getId() == id)
+                return apartment;
+        }
+        return null;
+    }
+
+    public void add(Apartment apartment) {
+        apartments.add(apartment);
+    }
+
+    public void update(int id, Apartment updatedApartment){
+        Apartment apartment = get(id);
+        apartment.setApartmentNumber(updatedApartment.getApartmentNumber());
+        apartment.setApartmentCapacity(updatedApartment.getApartmentCapacity());
+        apartment.setApartmentPrice(updatedApartment.getApartmentPrice());
+        apartment.setApartmentStatus(updatedApartment.getApartmentStatus());
+
+    }
+
+    public List<Apartment> getApartments () {
+        return apartments;
+    }
+
+
 }
--- src/main/java/com/andersenlab/entity/Apartment.java
+++ src/main/java/com/andersenlab/entity/Apartment.java
@@ -1,16 +1,23 @@
 package com.andersenlab.entity;
 
+import java.util.Objects;
+
 public class Apartment {
     private int id;
     private int apartmentNumber;
-    private ApartmentType apartmentType;
+    private int apartmentCapacity;
+    private long apartmentPrice;
     private ApartmentStatus apartmentStatus;
 
-    public Apartment(int id, int apartmentNumber, ApartmentType apartmentType) {
+    public Apartment() {
+    }
+
+    public Apartment(int id, int apartmentNumber, int apartmentCapacity, long apartmentPrice, ApartmentStatus apartmentStatus) {
         this.id = id;
         this.apartmentNumber = apartmentNumber;
-        this.apartmentType = apartmentType;
-        apartmentStatus = ApartmentStatus.AVAILABLE;
+        this.apartmentCapacity = apartmentCapacity;
+        this.apartmentPrice = apartmentPrice;
+        this.apartmentStatus = apartmentStatus;
     }
 
     public int getId() {
@@ -29,12 +36,20 @@ public class Apartment {
         this.apartmentNumber = apartmentNumber;
     }
 
-    public ApartmentType getApartmentType() {
-        return apartmentType;
+    public int getApartmentCapacity() {
+        return apartmentCapacity;
     }
 
-    public void setApartmentType(ApartmentType apartmentType) {
-        this.apartmentType = apartmentType;
+    public void setApartmentCapacity(int apartmentCapacity) {
+        this.apartmentCapacity = apartmentCapacity;
+    }
+
+    public long getApartmentPrice() {
+        return apartmentPrice;
+    }
+
+    public void setApartmentPrice(long apartmentPrice) {
+        this.apartmentPrice = apartmentPrice;
     }
 
     public ApartmentStatus getApartmentStatus() {
@@ -43,5 +58,29 @@ public class Apartment {
 
     public void setApartmentStatus(ApartmentStatus apartmentStatus) {
         this.apartmentStatus = apartmentStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apartment apartment = (Apartment) o;
+        return id == apartment.id && apartmentNumber == apartment.apartmentNumber && apartmentCapacity == apartment.apartmentCapacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, apartmentNumber, apartmentCapacity);
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "id=" + id +
+                ", apartmentNumber=" + apartmentNumber +
+                ", apartmentCapacity=" + apartmentCapacity +
+                ", apartmentPrice=" + apartmentPrice +
+                ", apartmentStatus=" + apartmentStatus +
+                '}';
     }
 }
