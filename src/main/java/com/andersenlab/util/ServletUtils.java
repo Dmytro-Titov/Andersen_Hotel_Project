8b959f91dea5f71717e8a2d63e596c51628748f0
Dmytro Titov <octopusless@gmail.com>
2023-08-18 19:07:54 +0300

    Remaster connection pool v3

--- src/main/java/com/andersenlab/util/ServletUtils.java
+++ src/main/java/com/andersenlab/util/ServletUtils.java
@@ -7,13 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class ServletUtils {
-    public static HotelFactory getHotelFactoryInstance() {
-        var configData = ConfigHandler.createConfig("src/main/resources/config/config-dev.yaml");
-        Config config = new Config();
-        config.setConfigData(configData);
-        HotelFactory hotelFactory = new HotelFactory(config);
-        return hotelFactory;
-    }
     public static String readBody(BufferedReader reader) throws IOException {
         StringBuilder jsonBody = new StringBuilder();
         String line;
